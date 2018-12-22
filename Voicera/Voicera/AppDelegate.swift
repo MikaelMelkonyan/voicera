220d37b5d01a393a3ab99942b5dd04fceed1bd02
Mikael Melkonyan <melkonyan2311@mail.ru>
2018-12-22 16:01:38 +0200

    Created project

---
+++ Voicera/Voicera/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Voicera
+//
+//  Created by Mikael on 12/22/18.
+//  Copyright © 2018 Mikael-Melkonyan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
