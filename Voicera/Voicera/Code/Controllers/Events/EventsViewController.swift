8f1c0cef9ef1b2418caea4382fcd19ced2cc330d
Mikael Melkonyan <melkonyan2311@mail.ru>
2018-12-22 16:40:06 +0200

    Launch screen changes

---
+++ Voicera/Voicera/Code/Controllers/Events/EventsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EventsViewController.swift
+//  Voicera
+//
+//  Created by Mikael on 12/22/18.
+//  Copyright © 2018 Mikael-Melkonyan. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController {
+    
+    
+}
